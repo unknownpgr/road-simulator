25be369c667dd0730710687040feb92b49c4f364
unknownpgr <unknownpgr@gmail.com>
2020-11-09 16:32:21 +0900

    Implement full lane with moving camera

--- thick.js
+++ thick.js
@@ -1,34 +1,34 @@
 function createThickLine(points, thickness) {
-  const EPSILON = 0.00001;
+  const EPSILON = 0.0001;
+  const t = thickness / 2;
+  const n = points.length;
+  const R = Math.PI / 2;
 
-  let n = points.length;
-  let R = Math.PI / 2;
   let segments = [], left = [], right = [];
 
   for (let i = 0; i < n - 1; i++)segments.push(points[i + 1].sub(points[i]));
 
   // Start points
-  left.push(points[0].add(segments[0].rot(R).norm().mul(thickness)));
-  right.push(points[0].add(segments[0].rot(-R).norm().mul(thickness)));
+  left.push(points[0].add(segments[0].rot(R).norm().mul(t)));
+  right.push(points[0].add(segments[0].rot(-R).norm().mul(t)));
 
   // Middle points
   for (let i = 0; i < n - 2; i++) {
-    let sint = segments[i].crs(segments[i + 1]) / (segments[i].len() * segments[i + 1].len()); // sin(theta); theta = angle between segment[i] and segment[i+1]
-
+    const sint = segments[i].crs(segments[i + 1]) / (segments[i].len() * segments[i + 1].len()); // sin(theta); theta = angle between segment[i] and segment[i+1]
     if (Math.abs(sint) < EPSILON) {
-      left.push(points[i + 1].add(segments[i].rot(R).norm().mul(thickness)));
-      right.push(points[i + 1].add(segments[i].rot(-R).norm().mul(thickness)));
+      left.push(points[i + 1].add(segments[i].rot(R).norm().mul(t)));
+      right.push(points[i + 1].add(segments[i].rot(-R).norm().mul(t)));
     } else {
-      let unit = segments[i].norm().sub(segments[i + 1].norm());
-      let d = unit.mul(thickness).div(sint); // d is 'right' side vector.0
+      const unit = segments[i].norm().sub(segments[i + 1].norm());
+      const d = unit.mul(t).div(sint); // d is 'right' side vector.0
       right.push(points[i + 1].add(d));
       left.push(points[i + 1].sub(d));
     }
   }
 
   // End points
-  left.push(points[n - 1].add(segments[n - 2].rot(R).norm().mul(thickness)));
-  right.push(points[n - 1].add(segments[n - 2].rot(-R).norm().mul(thickness)));
+  left.push(points[n - 1].add(segments[n - 2].rot(R).norm().mul(t)));
+  right.push(points[n - 1].add(segments[n - 2].rot(-R).norm().mul(t)));
 
   return [left, right];
 }
