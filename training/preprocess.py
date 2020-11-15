24f51520116f50e4a95a84b9ba8ee63a9b5f20ad
unknownpgr <unknownpgr@gmail.com>
2020-11-16 02:42:12 +0900

    Update preprocessor

--- training/preprocess.py
+++ training/preprocess.py
@@ -1,15 +1,32 @@
 import os
 import cv2
+import csv
+import numpy as np
 
-raw_dir = '../dataset/imgs'
-for f in os.listdir(raw_dir):
-    full_path = raw_dir+'/'+f
-    print(f)
-    # Read image
-    img = cv2.imread(full_path)
-    # Resize image
-    img = cv2.resize(img, (120, 90))
-    # Cut image half (=RoI)
-    img = img[60:, :]
-    # Save
-    cv2.imwrite('data/'+f, img)
+
+def load_data(dir):
+    rows = []
+    for f in os.listdir(dir):
+        full_path = os.path.join(dir, f)
+        # Split file name into labels
+        pos, angle = f[:-4].split('_')
+        print(pos, angle)
+        # Read image
+        img = cv2.imread(full_path)
+        # Convert color
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Resize image
+        img = cv2.resize(img, (640//16, 480//16))
+        # Convert image to array
+        img = list(np.reshape(img, [-1]))
+        # Add to row
+        rows.append([pos, angle]+img)
+    return rows
+
+
+with open("./data/dataset.csv", 'w', encoding='utf-8', newline='') as f:
+    w = csv.writer(f)
+    root = '../dataset'
+    for dir in os.listdir(root):
+        rows = load_data(os.path.join(root, dir))
+        w.writerows(rows)
